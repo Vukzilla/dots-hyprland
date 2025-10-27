088a9a43f4cdb5398d604ca51a8d82e7f43e836a
Vukzilla <vukzilla@gmail.com>
2025-10-27 20:19:07 +0100

    Added xorg-xhost

--- install.sh
+++ install.sh
@@ -12,11 +12,12 @@ fi
 sudo sed -i 's/^#\(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
 sudo locale-gen
 
-sudo pacman -S --needed hyprland mission-center fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-skk qt6ct qt5ct alacritty exa sdl2 glfw hypridle hyprcursor swww yad pipewire pipewire-pulse wireplumber xdg-desktop-portal xdg-desktop-portal-hyprland gtk3 fcitx5-configtool ttf-jetbrains-mono-nerd noto-fonts-cjk ttf-hack bluez bluez-utils blueman pavucontrol networkmanager network-manager-applet nautilus grim slurp jq wl-clipboard hyprpicker hyprlock noto-fonts-emoji waybar swaync fish materia-gtk-theme qt5-wayland qt6-wayland fastfetch polkit-gnome wofi file-roller gvfs-mtp hyprshot wqy-zenhei guvcview
+sudo pacman -S --needed hyprland mission-center fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-skk qt6ct qt5ct alacritty exa sdl2 glfw hypridle hyprcursor swww yad pipewire pipewire-pulse wireplumber xdg-desktop-portal xdg-desktop-portal-hyprland gtk3 fcitx5-configtool ttf-jetbrains-mono-nerd noto-fonts-cjk ttf-hack bluez bluez-utils blueman pavucontrol networkmanager network-manager-applet nautilus grim slurp jq wl-clipboard hyprpicker hyprlock noto-fonts-emoji waybar swaync fish materia-gtk-theme qt5-wayland qt6-wayland fastfetch polkit-gnome wofi file-roller gvfs-mtp hyprshot wqy-zenhei guvcview xorg-xhost
 
 $PKG_MGR -S --needed hyprshade python-pywal16 wlogout emote
 
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
+xhost +SI:localuser:root
 systemctl --user enable --now hypridle.service
 sudo systemctl enable bluetooth.service
 
